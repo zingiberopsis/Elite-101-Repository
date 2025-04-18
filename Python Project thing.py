9f43b36b0c091582123abb89bef218183cf1d83d
zingiberopsis <scottm112008@gmail.com>
2025-04-18 21:56:21 +0000

    More updates!

--- Python Project thing.py
+++ Python Project thing.py
@@ -3,13 +3,13 @@ name = input("What's your name? ")
 print("It's very nice to meet you " + name + "! So, how can I help you?")
 def display_options():
     print("1: Create Account ")
-    print("2: Modify Account **COMING SOON**")
+    print("2: Modify Account **TEMPORARILY DOWN**")
     print("3: Check Balance ")
     print("4: Deposit ")
     print("5: Withdraw ")
     print("6: Delete Account ")
     print("7: Exit System")
-
+display_options    
 def user_selection():
     while(True):
         display_options()
@@ -19,26 +19,25 @@ def user_selection():
             while(True):
                 username = input("Enter your username.")
                 password = str(input("Hello " + username + ". What will your password be?"))
-                    print("Thank you for making an account, " + username + "!")
+                print("Thank you for making an account, " + username + "!")
         elif(number == "2"):
-            print("We apologize, but this side of the app is under construction!")
+            print("We apologize, but this side of the app is currently disabled!")
             return
         elif(number == "3"):
-            def balance():
-
+            print("PLACEHOLDER")
         elif(number == "4"):
-           def deposit(user, amount):
-        user.balance += amount
-        print(f"Deposited ${amount}. New balance: ${user.balance}")
-                if 
+            def deposit(user, amount):
+                user.balance += amount
+                print(f"Deposited ${amount}. New balance: ${user.balance}")
         elif(number == "5"):
-             def withdraw(user, amount):
-        if amount > user.balance:
-            print("Insufficient funds.")
-        else:
-            user.balance -= amount
+            def withdraw(user, amount):
+                if amount > user.balance:
+                    print("Insufficient funds.")
+                else:
+                    user.balance -= amount
             print(f"Withdrew ${amount}. New balance: ${user.balance}")
         elif(number == "6"):
+            print("Are you sure you want to delete your account? YES, or NO?")
         elif(number == "7"):
             quit
- user_selection()
+    user_selection()
